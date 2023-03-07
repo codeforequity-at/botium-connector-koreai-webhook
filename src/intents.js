c3b3de49b4e3c433018332575a5bd92fe1bc6ece
Attila Ujj <attila.ujj@botium.at>
2023-03-07 09:48:56 +0100

    BOT-3298  upload/download/nlp

--- src/intents.js
+++ src/intents.js
@@ -108,11 +108,11 @@ const getContent = async ({ container, statusCallback }) => {
   return resDownload.data
 }
 
-const importKoreaiIntents = async ({ caps, importallutterances, buildconvos }, {}) => {
+const importKoreaiIntents = async ({ caps, importallutterances, buildconvos }, {statusCallback}) => {
   const driver = new BotDriver(getCaps(caps))
   const container = await driver.Build()
   await container.Start()
-  const chatbotData = await getContent({ container })
+  const chatbotData = await getContent({ container, statusCallback })
 
   const utterances = {}
 
